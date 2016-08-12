7d67f995b08b9bc655c47264283c9eec23d67309
Michael Egger <me@anyma.ch>
2016-08-12 17:53:51 +0200

    servos and pwm working

--- dmx-servos.ino
+++ dmx-servos.ino
@@ -1,20 +1,41 @@
+#include <Servo.h>
 #include <DMXSerial.h>
 #include <EEPROM.h>
 #include "Timer.h"	 //http://github.com/JChristensen/Timer
-Timer t;
+
+#define PWM 1
+#define SERVO 2
 
 // ================================================================================================
 //																			ADJUST THESE VALUES
 
+#define DMX_ADDRESS				10
+#define	CAN_CHANGE_DMX_ADDRESS	1
 #define DISPLAY_BRIGHTNESS		14			// values: 0 - 16
+
+
+const char modes [] =  			{	SERVO	, 	SERVO	, 	SERVO	, 	PWM		, 	PWM		, 	PWM		};
+const int lows [] = 			{	700		,	1000	,	1000	,	1000	,	1000	,	1000	};
+const int highs [] = 			{	2290	,	2000	,	2000	,	2000	,	2000	,	2000	};
+
+const int start[] = 			{	1500	,	1500	,	1500	,	1500	,	1500	,	1500	};
+const char smooth [] = 			{	4		,	0		,	3		,	16		,	0		,	0		};
 
 // ================================================================================================
 // ================================================================================================
 //																			DONT CHANGE BELOW...
+
+const char 		out_pins[] = {3,5,6,9,10,11};		// Servos are on pins 3,5,6 9,10,11
+
 unsigned int 	dmx_address;
+unsigned char 	display[3];
+float			dmx_buffer[6];
+int 			out_buffer[6];
 unsigned long 	time_since_last_dmx_packet;
 unsigned char 	button_state;
-
+Timer 			t;
+Servo 			myservo[6];
+unsigned char 	out_idx;
 
 // ==========================================================================================
 //														SETUP
@@ -27,7 +48,9 @@ void setup() {
 	DDRE = ~B10111111;
 	
 	// retrieve dmx address from EEPROM
-	dmx_address = 0;
+	dmx_address = DMX_ADDRESS;
+	
+#if CAN_CHANGE_DMX_ADDRESS
 	unsigned char lowbyte,highbyte,controlbyte;
 	lowbyte = EEPROM.read(0);
 	highbyte = EEPROM.read(1);
@@ -37,17 +60,31 @@ void setup() {
 			dmx_address = lowbyte | (highbyte << 8 );
 		} 
   	};
+	int_to_display(dmx_address);		// write dmx adress to display buffer 
+#endif
 	
 	DMXSerial.init(DMXReceiver);
+	time_since_last_dmx_packet = DMXSerial.noDataSince();
+	delay(1000);
+
+	for (char i = 0; i < 6; i++) {
+		if (modes[i] == SERVO) {
+				myservo[i].attach(out_pins[i]);
+				out_buffer[i] = start[i];
+				myservo[i].writeMicroseconds(out_buffer[i]);
+		}
+	}
 
 	//pullups on pushbuttons. arduino style
 	pinMode(13,INPUT_PULLUP);
 	pinMode(A0,INPUT_PULLUP);
 
 	// set up timed events
+#if CAN_CHANGE_DMX_ADDRESS
 	t.every (1, update_display);
 	t.every (20, check_btns);
-	t.every (20, check_dmx);
+#endif
+	t.every (10, check_dmx);
 }
 
 // ==========================================================================================
@@ -56,6 +93,16 @@ void setup() {
 
 void loop() {
 	t.update();
+	
+	out_idx++;
+	out_idx %= 6;
+	
+	if (modes[out_idx] == SERVO) {
+		myservo[out_idx].writeMicroseconds(out_buffer[out_idx]);
+	} else {
+		analogWrite(out_pins[out_idx], out_buffer[out_idx]);
+	}
+
 }
 
 // ==========================================================================================
@@ -67,10 +114,30 @@ void loop() {
 
 void check_dmx() {
 	time_since_last_dmx_packet = DMXSerial.noDataSince();
+	unsigned char i;
+	i= 0;
 	
 	if (time_since_last_dmx_packet < 1000) {
+
+		unsigned int temp;
 		
+		for (i = 0; i < 6; i++) {
+			
+			temp = DMXSerial.read(dmx_address + i);
+
+			dmx_buffer[i] = ((float)smooth[i] * dmx_buffer[i] + (float)temp) / (float)(smooth[i] + 1);
+			temp = round(dmx_buffer[i]);
+
+			//int_to_display(temp);
+	
+			if (modes[i] == SERVO) {
+				temp = map(temp, 0, 255, lows [i], highs[i]);
+			}
+			
+			out_buffer[i] = temp;
+		}
 	}
+
 }
 
 // ------------------------------------------------------------------------------------------
@@ -88,6 +155,7 @@ void up_or_down(unsigned char state) {
 	if (state & (1 << 0)) dmx_address--; 
 	else if (state & (1 << 1)) dmx_address++; 
 	dmx_address %= 512;
+	int_to_display(dmx_address);
 }
 
 // ------------------------------------------------------------------------------------------
@@ -147,13 +215,23 @@ unsigned char get_digit(unsigned char d, unsigned int n) {
 }
 
 // ------------------------------------------------------------------------------------------
+// write a number to display buffer
+
+void int_to_display (unsigned int number) {
+	for (char i = 0; i < 3; i++) {
+		display[i] = leds_for_digit(get_digit(i,number));
+	}
+}
+
+// ------------------------------------------------------------------------------------------
 // refresh display (called by timer every millisecond)
 
 void update_display(void) {
+
 		static unsigned char d;
 		d++;
 		d %= 3 + (16 - DISPLAY_BRIGHTNESS);			// adjust brightness here: lower numbers = brighter display. try values from 3 - 30
-		if (d < 3)	display_digit(d, get_digit(d,dmx_address + 1));
+		if (d < 3)	display_set_leds(d);
 		else PORTF &= B10001111;	// turn all	 digits off
 		
 		if (button_state == 0) { 		// dont blink when buttons are pressed
@@ -178,12 +256,18 @@ void update_display(void) {
 //					e - PD6
 //					f - PD4
 //					g - PF1
-void display_set_leds( unsigned char l ) {
+void display_set_leds( unsigned char n ) {
+	
+	n %= 3;
 
-// turn all LEDS off first
+	PORTF &= B10001111;	 // turn all cathodes off
+						 // turn all anodes off
 	PORTD &= B10100101;
 	PORTE &= B10111111;
 	PORTF &= B11111100;
+	
+	unsigned char l;
+	l = display[n];
 	
 	if ( l & (1 << 0 )) { PORTD |= ( 1 << 1 ); }
 	if ( l & (1 << 1 )) { PORTD |= ( 1 << 3 ); }
@@ -192,26 +276,24 @@ void display_set_leds( unsigned char l ) {
 	if ( l & (1 << 4 )) { PORTD |= ( 1 << 6 ); }
 	if ( l & (1 << 5 )) { PORTD |= ( 1 << 4 ); }
 	if ( l & (1 << 6 )) { PORTF |= ( 1 << 1 ); }
+	
+	PORTF |= (1 << ( n + 4)); // turn on cathode
 }
 
-void display_digit( unsigned char n, unsigned char d ) {
-	n %= 3;
-	d %= 16;
-	PORTF &= B10001111;	 // turn all	digits off
+unsigned char leds_for_digit(unsigned char d ) {
 	switch(d) {
-		case 0: display_set_leds(0x3f); break;
-		case 1: display_set_leds(0x06); break;
-		case 2: display_set_leds(0x5b); break;
-		case 3: display_set_leds(0x4f); break;
-		case 4: display_set_leds(0x66); break;
-		case 5: display_set_leds(0x6d); break;
-		case 6: display_set_leds(0x7d); break;
-		case 7: display_set_leds(0x07); break;
-		case 8: display_set_leds(0x7f); break;
-		case 9: display_set_leds(0x6f); break;
-		default: display_set_leds(0x00); break;
+		case 0: return 0x3f;
+		case 1: return 0x06;
+		case 2: return 0x5b;
+		case 3: return 0x4f;
+		case 4: return 0x66;
+		case 5: return 0x6d;
+		case 6: return 0x7d;
+		case 7: return 0x07;
+		case 8: return 0x7f;
+		case 9: return 0x6f;
+		default: return 0x00;
 	}
-	PORTF |= (1 << ( n + 4)); 
 }
 
 
